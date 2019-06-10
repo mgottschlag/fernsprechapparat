0066e34da3e1e4dce4bd9f252e0679a53254041d
Mathias Gottschlag <mgottschlag@gmail.com>
2019-06-10 11:23:54 +0200

    Crate documentation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//! Application which implements SIP support for modded old telephones with
+//! rotary dials.
+//!
+//! This program is intended for a mod of a FeTAp (Fernsprechtischapparat) of
+//! the Deutsche Bundespost, but will likely work with any similar phones.
+
 mod gpio;
 
 fn main() {
